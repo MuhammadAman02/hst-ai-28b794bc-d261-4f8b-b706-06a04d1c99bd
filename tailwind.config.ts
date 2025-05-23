cfe2cbb82a4b8ff762ae1c581d2cb42000203d12
MuhammadAman02 <mohammada@hst.ie>
2025-05-23 17:05:43 +0500

    update file

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#0000FF', // Adjust this hex code if needed to match the image
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
